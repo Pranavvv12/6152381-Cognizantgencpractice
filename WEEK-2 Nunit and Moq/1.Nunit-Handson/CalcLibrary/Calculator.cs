a855c9a28b08e280174af0bee623a5612622dd23
Pranavvv12 <pranavk2004915@gmail.com>
2025-06-29 19:54:07 +0530

    final

--- WEEK-2 Nunit and Moq/1.Nunit-Handson/CalcLibrary/Calculator.cs
+++ WEEK-2 Nunit and Moq/1.Nunit-Handson/CalcLibrary/Calculator.cs
@@ -7,6 +7,7 @@ namespace CalcLibrary
             return a + b;
         }
 
+        // Placeholder for future operations
         public int Subtract(int a, int b)
         {
             return a - b;
