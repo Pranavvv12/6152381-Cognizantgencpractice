8ce52daa9dde00baeb19afb7018ef5216c14dcf8
Pranavvv12 <pranavk2004915@gmail.com>
2025-07-27 19:22:44 +0530

    Updated Ex 1 and 2

---
+++ WEEK-6 REACT/2.ReactJS-HOL/REACT-2/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+function App() {
+  
+
+  return (
+    <>
+    <Home/>
+    <About/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App
